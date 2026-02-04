2b655334d3851e0dabf001ec870861c0009fe89a
Rob Pritchard <rob_pritchard@outlook.com>
2026-02-04 11:41:58 +0000

    Update Postmark API token

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :postmark
 
   config.action_mailer.postmark_settings = {
-    api_token: ENV["POSTMARK_API_TOKEN"] || Rails.application.credentials.postmark_api_token
+    api_token: Rails.application.credentials.postmark_api_token || ENV["POSTMARK_API_TOKEN"]
   }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
