a6ceb5855ad0cb5111b07743d0ae95002b2d0360
Hans-Ulrich Hölscher <vaxorcist@googlemail.com>
2026-02-13 10:06:00 +0100

    Fix Rubocop style violations and add missing serial_number to fixture

--- db/migrate/20260212135907_make_serial_number_required.rb
+++ db/migrate/20260212135907_make_serial_number_required.rb
@@ -1,4 +1,4 @@
-# decor/db/migrate/20260212135907_make_serial_number_required.rb - version 1.0
+# decor/db/migrate/20260212135907_make_serial_number_required.rb - version 1.1
 # Makes serial_number field required (not null) for computers table
 
 class MakeSerialNumberRequired < ActiveRecord::Migration[8.1]
